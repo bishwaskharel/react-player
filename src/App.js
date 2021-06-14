ebef427f3cf83e9eb990d6853e99454403e77893
bishwaskharel <bishukharel98@gmail.com>
2021-06-14 15:49:28 +0545

    Player UI set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 // Adding Styles
-
 import "./styles/app.scss";
 
 // Adding Compoments
 import Player from "./components/Player";
 import Song from "./components/Song";
+
+// Import Util
+import data from "./util"
 
 function App() {
   return (
---
+++ src/util.js
@@ -0,0 +1,68 @@
+import { v4 as uuidv4 } from "uuid";
+
+const chillHop = () => {
+  return [
+    {
+      name: "Higher",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-300x300.jpg",
+      artist: "Misha, Jussi Halme",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15088",
+      color: ["#2E385B", "#EB7476"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Lower",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-300x300.jpg",
+      artist: "Heppa, Movilake",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12091",
+      color: ["#1E385B", "#E87476"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Higher",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-300x300.jpg",
+      artist: "Misha, Jussi Halme",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15088",
+      color: ["#2E385B", "#EB7476"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Lower",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-300x300.jpg",
+      artist: "Heppa, Movilake",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12091",
+      color: ["#1E385B", "#E87476"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Higher",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-300x300.jpg",
+      artist: "Misha, Jussi Halme",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15088",
+      color: ["#2E385B", "#EB7476"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Lower",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-300x300.jpg",
+      artist: "Heppa, Movilake",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12091",
+      color: ["#1E385B", "#E87476"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+};
+
+export default chillHop;
