ac59414fdf9a324320acfa1f4e3d95c47e257fc4
Bishwas Kharel <bishukharel98@gmail.com>
2022-09-16 16:24:16 +0545

    new images updated

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ const chillHop = () => {
     {
       name: "K Garu",
       cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t1.6435-9/174013060_2993555950968637_4608869875148576841_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=174925&_nc_ohc=4fUZj4DCSMAAX9lruma&tn=gsHzJ0wWvakh9JdO&_nc_ht=scontent.fktm6-1.fna&oh=a0aff7016126848aa05f3a560e6ee1f3&oe=60CD5D68",
+"https://i.scdn.co/image/ab67616d00001e0205fac6e4a7376c8fa9c46a66",
       artist: "Apurva Tamang",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FApurva%20Tamang%20-%20K%20Garu%20(Lyrics)%20SAD.mp3?alt=media&token=85e5b206-493d-4073-96ac-c54093d511ce",
@@ -16,7 +16,7 @@ const chillHop = () => {
     {
       name: "Hawayein",
       cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t1.6435-9/159646164_282123859938527_796713823172858711_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=174925&_nc_ohc=lb8elJbK20IAX-CgL03&_nc_ht=scontent.fktm6-1.fna&oh=7404ef75f9186384f91dec4068c70f5c&oe=60CE5543",
+"https://a10.gaanacdn.com/gn_img/song/DwPKOBbqVZ/PKOQYzaqWq/size_l_1516002119.jpg",
       artist: "Arijit Singh",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FHawayein%20Jab%20Harry%20Met%20Sejal%20Anushka%20Sharma%20Shah%20Rukh%20Khan%20Pritam%20Imtiaz%20Ali%20Arijit%20Singh.mp3?alt=media&token=5dee28b6-a0ce-4231-8ba4-f5361e71b2ed",
@@ -26,8 +26,7 @@ const chillHop = () => {
     },
     {
       name: "Country Roads ",
-      cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t31.18172-8/12891776_10153596372188691_385732722906294546_o.jpg?_nc_cat=100&ccb=1-3&_nc_sid=174925&_nc_ohc=BP56Jpsrx0cAX8eOgaC&_nc_ht=scontent.fktm6-1.fna&oh=103a5b776c191e349b8154cab0b4b847&oe=60F2699B",
+      cover: "https://i.scdn.co/image/ab67616d0000b273a19cee952b6dae128d68f116",
       artist: "John Denver",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FJohn%20Denver%20-%20Take%20Me%20Home%2C%20Country%20Roads%20(Audio).mp3?alt=media&token=66eaac9d-0beb-4784-a749-e19163c5ec5c",
@@ -37,8 +36,7 @@ const chillHop = () => {
     },
     {
       name: "Man Ka Bhawana",
-      cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t1.6435-9/183353887_3008744559449776_2551974513509611375_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=nn-DFnj3wZkAX-eDYm9&_nc_ht=scontent.fktm6-1.fna&oh=ef33ab21131952c11092bced4a3c9fed&oe=60CE677E",
+      cover:"https://i0.wp.com/neplych.com/wp-content/uploads/2020/08/apurva-tamang.jpg?fit=1080%2C997&ssl=1",
       artist: "Apurva Tamang",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FMann%20Ka%20Bhawana%20(Lyrics)%20-%20Apurva%20Tamang%20(feat.%20Shaktay).mp3?alt=media&token=aae43448-ee1a-4c41-a8af-5335c86d76b3",
@@ -48,8 +46,7 @@ const chillHop = () => {
     },
     {
       name: "Say you won't let go",
-      cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t1.6435-9/70014927_2632094220175855_2302866824510832640_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=174925&_nc_ohc=FdOVIErQRSwAX-JbO5e&tn=gsHzJ0wWvakh9JdO&_nc_ht=scontent.fktm6-1.fna&oh=ae7c141b73373e06652324e49b45c622&oe=60D099B6",
+      cover:"https://i1.sndcdn.com/artworks-000336807999-lwhb3x-t500x500.jpg",
       artist: "James Arthur",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FJames%20Arthur%20-%20Say%20You%20Won't%20Let%20Go.mp3?alt=media&token=081675b3-c93e-41fd-b953-f742a13960cc",
@@ -59,8 +56,7 @@ const chillHop = () => {
     },
     {
       name: "Lae Dooba",
-      cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t1.6435-9/128449249_212089463644357_2326235491085829543_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=u6DTEibuicUAX8RU2SE&_nc_ht=scontent.fktm6-1.fna&oh=32596e0607376892a2c9e7eec42c5264&oe=60CE1DC4",
+      cover:"https://c-fa.cdn.smule.com/rs-s80/arr/a5/21/c2d1a994-9af5-420f-a56c-91045c5d43e4_1024.jpg",
       artist: "Rochak Kohli",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FLae%20Dooba%20-%20Lyrical%20Aiyaary%20Sidharth%20Malhotra%2C%20Rakul%20Preet%20Sunidhi%20Chauhan%20Rochak%20Kohli.mp3?alt=media&token=dd3feed1-0fc4-4e6f-b667-176cd6b503db",
@@ -70,8 +66,7 @@ const chillHop = () => {
     },
     {
       name: "Sunideu",
-      cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t1.6435-9/127034412_2882812985376268_233657191342582010_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=174925&_nc_ohc=F6Nwdwe_qpwAX8V6F7q&_nc_ht=scontent.fktm6-1.fna&oh=59e780c8d48f57da95d43ab8c20f48bb&oe=60CE45F1",
+      cover:"https://www.lyricalhub.com/wp-content/uploads/2020/02/Yellow-Sound-Wave-Icon-Band-Logo-92.png",
       artist: "Apurva Tamang",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FSunideu%20-%20Apurva%20Tamang%20(Official%20Video).mp3?alt=media&token=c9dd7e74-7319-45ef-aa21-7e0046e3253a",
@@ -81,8 +76,7 @@ const chillHop = () => {
     },
     {
       name: "Back To December",
-      cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t31.18172-8/20988198_10154975234150369_1493436770276743217_o.jpg?_nc_cat=101&ccb=1-3&_nc_sid=174925&_nc_ohc=d6IvgI0SmUUAX-QnqiR&_nc_ht=scontent.fktm6-1.fna&oh=a0b4d9b49ea5bcb3fd5634704a885915&oe=60EEFD6A",
+      cover: "https://i.pinimg.com/originals/2c/ea/5c/2cea5c632449e73d692d8c1ff5cf31fa.jpg",
       artist: "Taylor Swift",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FTaylor%20Swift%20-%20Back%20To%20December.mp3?alt=media&token=a71721b6-c47d-4ec4-a7c8-b6f92a73931e",
@@ -92,8 +86,8 @@ const chillHop = () => {
     },
     {
       name: "Gahiro Gahiro",
-      cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t1.6435-9/178448043_313525990127640_3008285912566297693_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=174925&_nc_ohc=N-hk9L_4UpUAX8HxJsY&_nc_ht=scontent.fktm6-1.fna&oh=d2d8b3ad90822a090349ab0c3aca85a6&oe=60CF03F1",
+      cover:"https://images.genius.com/52a98fb10dc0147b80f2eba446d714bd.630x630x1.jpg",
+
       artist: "Bipul Chhetri",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FBipul%20Chettri%20-%20Gahiro%20Gahiro%20(Single).mp3?alt=media&token=e3562832-98ee-4c1a-9f8b-b9e66cbc59f1",
@@ -103,8 +97,7 @@ const chillHop = () => {
     },
     {
       name: "Timrai Tira",
-      cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t1.6435-9/132991536_2909757922681774_4497083639034569593_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=174925&_nc_ohc=1_stPRxavBgAX9jjw9E&_nc_ht=scontent.fktm6-1.fna&oh=e38227cad8f6d509b7f4017ad981e538&oe=60CD90C0",
+      cover:"https://c.saavncdn.com/821/Timrai-Tira-Singe-Nepali-2020-20200617165210-500x500.jpg",
       artist: "Apurva Tamang",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FTimrai%20TiraApurva%20Tamang%20Lyrics%20vedio%20-%20Paradise.mp3?alt=media&token=9dae5494-242c-4027-9937-385ee8504638",
@@ -114,8 +107,7 @@ const chillHop = () => {
     },
     {
       name: "Party In The U.S.A.",
-      cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t1.6435-9/91241574_10158339969677147_5478575534482915328_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=174925&_nc_ohc=nZnTFbzHSYIAX9-fwVQ&_nc_ht=scontent.fktm6-1.fna&oh=215fbc3da0e3d1856711eb2943b95d9a&oe=60CE1291",
+      cover: "https://i1.sndcdn.com/artworks-000561650619-x4y3z4-t500x500.jpg",
       artist: "Miley Cyrus",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FMiley%20Cyrus%20-%20Party%20In%20The%20U.S.A.%20(Official%20Video).mp3?alt=media&token=d04ca59b-a4af-4bb8-8488-9a485a99c0cc",
@@ -125,8 +117,8 @@ const chillHop = () => {
     },
     {
       name: "Tera Fitoor",
-      cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t31.18172-8/26756640_1970623356510214_5327136223752977562_o.jpg?_nc_cat=111&ccb=1-3&_nc_sid=174925&_nc_ohc=HfkugH2mescAX9ve4im&_nc_ht=scontent.fktm6-1.fna&oh=d0fc5f0886ee627f2d4903fa1b10f7b0&oe=60EE4E08",
+      cover:"https://i.scdn.co/image/ab67616d0000b273908fec17584651a31b73b97d",
+
       artist: "Himesh Reshammiya",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FTera%20Fitoor%20Lyrical%20-%20Genius%20Utkarsh%20Sharma%2C%20Ishita%20Chauhan%20Arijit%20Singh%20Himesh%20Reshammiya.mp3?alt=media&token=931e2782-6395-46a2-ae5e-443ce81b47d1",
@@ -136,8 +128,8 @@ const chillHop = () => {
     },
     {
       name: "Sano",
-      cover:
-        "https://scontent.fktm6-1.fna.fbcdn.net/v/t1.6435-9/93353801_2670754529915449_8071453132233637888_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=174925&_nc_ohc=bz4v_MJL-L4AX9Q04Uy&_nc_oc=AQkY0P9aER-DLFFpmXnpxSQFYy-zeiZhv2Q0aA4D2VSfhcLstl-1c5kgJ9i0vsBPMq28QxgoWlcw2mg0jc0QeX_i&tn=gsHzJ0wWvakh9JdO&_nc_ht=scontent.fktm6-1.fna&oh=84aa721fc07601b4272499c0d79070ba&oe=60CDD38C",
+      cover:"https://images.genius.com/f21e1843f087dc53a161fee34b0815de.1000x1000x1.jpg",
+
       artist: "Apurva Tamang",
       audio:
         "https://firebasestorage.googleapis.com/v0/b/image-upload-and-show-9ddea.appspot.com/o/Songs%2FSano%20-%20Apurva%20Tamang%20(Lyric%20Video).mp3?alt=media&token=d4bfde64-105a-4c3e-b8f6-6c082833091d",
